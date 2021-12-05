1ac06254b7de8b9643bf97fe4771d42d57581b05
lishujie18 <lishujie@umich.edu>
2021-12-04 23:28:20 -0500

    Update AddActivity.js

--- Components/AddActivity.js
+++ Components/AddActivity.js
@@ -2,15 +2,22 @@ import React, { useEffect, useState } from 'react';
 import { FlatList, StyleSheet, Text, TouchableOpacity, View, Button } from 'react-native';
 import { Input } from 'react-native-elements';
 import { getDataModel } from './DataModel';
+import { Icon } from 'react-native-elements'
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 
 function AddActivity({ navigation }) {
 
 
-  const colors = ['#87b9e7', '#F29C98', '#A990DD', '#91cec2','#ffe0ab'];
+  const colors = ['#87b9e7', '#F29C98', '#A990DD', '#91cec2', '#ffe0ab'];
+  // const icons = ['game-controller-outline', 'desktop-outline', 'barbell-outline', 'paw-outline', 'color-palette-outline', 'bed-outline','restaurant-outline', 'library-outline', 'people-outline', 'ellipsis-horizontal-outline']
+  const icons = {dict: {'game-controller-outline': '#8962F8', 'desktop-outline': '#F78F8A', 'barbell-outline': '#5EA8EC', 'paw-outline': '#4AC2AA', 'color-palette-outline': '#FCC089',
+  'bed-outline': '#F46972', 'restaurant-outline': '#D0DD84', 'library-outline': '#7AD3DA', 'people-outline': '#DEBD9C', 'ellipsis-horizontal-outline': '#CDCDCD',}}
 
   const [inputText, setInputText] = useState('');
-  const [color, setColor] = useState(colors[0]);
+  const [color, setColor] = useState(icons.dict['game-controller-outline']);
+  // const [icon, setIcon] = useState(icons[0]);
+  const [icon, setIcon] = useState('game-controller-outline');
 
 
   
@@ -30,10 +37,10 @@ function AddActivity({ navigation }) {
               />
             </View>
 
-            <View style={styles.colorSelectionContainer}>
+            {/* <View style={styles.selectionContainer}>
               <Text style={styles.label}>Tag Color</Text>
 
-              <View style={styles.colorContainer}>
+              <View style={styles.selectContentContainer}>
                   {
                       colors.map((item)=>(
                           <View style={styles.colorCell}>
@@ -46,15 +53,77 @@ function AddActivity({ navigation }) {
                                       borderRadius: 30,
                                       backgroundColor: item,
                                       borderWidth: 5,
-                                      borderColor: item===color?'#E5E5E5': 'white',
+                                      borderColor: item===color?'#F0F0F0': 'white',
                                     }}>
                               </TouchableOpacity>
                           </View>
                       ))
                   }
+              </View>
+            </View> */}
 
+            <View style={styles.selectionContainer}>
+              <Text style={styles.label}>Category</Text>
+
+              <View style={styles.selectContentContainer}>
+                  {
+                      Object.keys(icons.dict).map((item)=>(
+                          <View style={styles.colorCell}>
+                              <TouchableOpacity
+                                onPress={() => {
+                                  setIcon(item);
+                                  setColor(icons.dict[item])}}
+                                style={{
+                                  display: 'flex',
+                                  justifyContent:'center',
+                                  alignItems: 'center',
+                                  width: 55,
+                                  height: 55,
+                                  borderRadius: 55,
+                                  borderWidth: 5,
+                                  backgroundColor: icons.dict[item],
+                                  borderColor: item===icon?'#E9E9FF': 'white',
+                                }}>
+                                <Icon name={item}
+                                    type='ionicon'
+                                    color='white'
+                                  />
+                              </TouchableOpacity>
+                          </View>
+                      ))
+                  }
               </View>
             </View>
+
+            {/* <View style={styles.selectionContainer}>
+              <Text style={styles.label}>Icon</Text>
+
+              <View style={styles.selectContentContainer}>
+                  {
+                      icons.map((item)=>(
+                          <View style={styles.colorCell}>
+                              <TouchableOpacity
+                                onPress={() => {
+                                  setIcon(item);}}
+                                style={{
+                                  display: 'flex',
+                                  justifyContent:'center',
+                                  alignItems: 'center',
+                                  width: 40,
+                                  height: 40,
+                                  borderRadius: 40,
+                                  backgroundColor: item===icon?'#F0F0F0': 'white',
+                                }}>
+                                <Icon name={item}
+                                    type='ionicon'
+                                    color='black'
+                                  />
+                              </TouchableOpacity>
+                          </View>
+                      ))
+                  }
+              </View>
+            </View> */}
 
           </View>
 
@@ -62,7 +131,9 @@ function AddActivity({ navigation }) {
             <TouchableOpacity
               style={styles.button1}
               onPress={() => {
-                dataModel.addItem({text: inputText, time: 0, color: {color}});
+                dataModel.addItem({text: inputText, time: 0, color: color, icon: icon});
+                setInputText('');
+                setIcon('game-controller-outline');
                 navigation.navigate("Home");
             }}>
               <Text style={styles.buttonText}>Add Task</Text>
@@ -71,6 +142,8 @@ function AddActivity({ navigation }) {
             <TouchableOpacity
               style={styles.button2}
               onPress={() => {
+                setInputText('');
+                setIcon('game-controller-outline');
                 navigation.navigate("Home");
             }}>
             <Text style={styles.buttonText}>Go Back</Text>
@@ -92,7 +165,7 @@ const styles = StyleSheet.create({
       fontFamily: 'Rubik',
     },
     activityContainer: {
-      marginVertical: 40,
+      marginBottom: 60,
       flex: 1.2,
       display: 'flex',
       flexDirection: 'column',
@@ -107,12 +180,13 @@ const styles = StyleSheet.create({
       alignItems: 'center',
     },
     inputContainer: {
-      height: '40%',
+      height: '30%',
       display: 'flex',
       flexDirection: 'column',
       justifyContent: 'space-around',
       alignItems: 'flex-start',
       marginHorizontal: 40,
+      marginVertical: 10,
     },
     inputText: {
      fontSize: 18,
@@ -145,20 +219,30 @@ const styles = StyleSheet.create({
       fontWeight: '500',
       letterSpacing: 0.5,
     },
-    colorSelectionContainer: {
-      height: '40%',
+    selectionContainer: {
+      height: '30%',
       display: 'flex',
       flexDirection: 'column',
       justifyContent: 'space-around',
-      marginTop: 40,
       marginHorizontal: 40,
+      marginVertical: 10,
     },
-    colorContainer: {
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'space-around',
+    selectContentContainer: {
+      display: 'flex',
+      flex: 1,
+      flexDirection: 'row',
+      flexWrap: 'wrap',
+      // justifyContent: 'space-around',
 
     },
+    colorCell: {
+      width: '20%',
+      height: '100%',
+      borderRadius: 40,
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+    }
 
 
  
